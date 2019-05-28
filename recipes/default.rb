3c1125490d68d6a22c8eb28e1bd631ed938f3315
Matt Ray <matthewhray@gmail.com>
2019-05-28 18:56:50 +0800

    initial prototype, work in progress

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,22 @@
 # Cookbook:: audit-artifactory
 # Recipe:: default
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+
+# iterate over hash of attributes
+node['audit-artifactory']['profiles'].keys.each do |name|
+  profile = node['audit-artifactory']['profiles'][name]
+  if profile['version'].nil?
+    version = 'latest'
+  else
+    version = profile['version']
+  end
+
+  artifactory_profile name do
+    base_url profile['base_url']
+    group profile['group']
+    repo profile['repo']
+    version version
+  end
+end
+
+include_recipe 'audit::default'

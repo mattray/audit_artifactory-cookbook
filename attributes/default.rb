f8ee41874187a2706b12b093fa4d92213ba58552
Matt Ray <matthewhray@gmail.com>
2019-10-25 14:43:24 +1100

    Removed audit cookbook attributes.
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,1 @@
-default['audit']['reporter'] = 'chef-automate'
-default['audit']['insecure'] = true
-default['audit']['server'] = ''
-default['audit']['token'] = ''
-default['audit']['profiles'] = {}
-
 default['audit-artifactory']['profiles'] = {}

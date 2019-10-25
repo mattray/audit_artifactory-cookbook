fad82f6c758e71e8c86f9a3f84181b13e6ec3bb5
Matt Ray <matthewhray@gmail.com>
2019-10-25 19:51:12 +1100

    More attributes for authentication and switch to Chef::HTTP for SSL
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,7 @@
 default['audit-artifactory']['profiles'] = {}
+default['audit-artifactory']['user'] = nil
+default['audit-artifactory']['password'] = nil
+default['audit-artifactory']['api_key'] = nil
+
+default['audit-artifactory']['cache_dir'] = "#{Chef::Config['file_cache_path']}/audit-artifactory"
+default['audit']['profiles'] = {} # required for writing profiles to the audit cookbook
--- resources/artifactory_profile.rb
+++ resources/artifactory_profile.rb
@@ -7,34 +7,33 @@ property :repo, String, required: true
 property :version, String, default: 'latest'
 
 action :install do
-  require 'net/http'
-  require 'uri'
-
-  artifact = new_resource.artifact
-  base_url = new_resource.base_url
-  group = new_resource.group
+  base_url = new_resource.base_url.chomp('/') # strip trailing /
   repo = new_resource.repo
+  group = new_resource.group
+  artifact = new_resource.artifact
   version = new_resource.version
+
+  cache_dir = node['audit-artifactory']['cache_dir']
+  directory cache_dir
 
   # get the version of the latest
   if version.eql?('latest')
-    url = "#{base_url}/api/search/latestVersion?g=#{group}&a=#{artifact}&repos=#{repo}"
-    version = Net::HTTP.get_response(URI.parse(url)).body
+    version = Chef::HTTP.new(base_url).get("/api/search/latestVersion?g=#{group}&a=#{artifact}&repos=#{repo}")
   end
 
-  file_url = "#{base_url}/#{repo}/#{group}/#{artifact}/#{artifact}-#{version}.tgz"
+  remote_url = "#{base_url}/#{repo}/#{group}/#{artifact}/#{artifact}-#{version}.tgz"
   # fileurl = "#{base_url}/#{repo}/#{group}/#{artifact}/#{artifact}-#{version}.tar.gz"
   # fileurl = "#{base_url}/#{repo}/#{group}/#{artifact}/#{artifact}-#{version}.zip"
 
-  directory "#{Chef::Config['file_cache_path']}/audit-artifactory/"
+  local_path = "#{cache_dir}/#{artifact}-#{version}.tgz"
 
   # download the artifact
-  remote_file "#{Chef::Config['file_cache_path']}/audit-artifactory/#{artifact}-#{version}.tgz" do
-    source file_url
+  remote_file local_path do
+    source remote_url
     mode '0600'
     action :create
   end
 
   # add the local file to the audit cookbook
-  node.default['audit']['profiles'][artifact] = { 'path' => "#{Chef::Config['file_cache_path']}/audit-artifactory/#{artifact}-#{version}.tgz" }
+  node.default['audit']['profiles'][artifact]['path'] = local_path
 end
